25c58b54bee91771553912ad254a81972b882b4c
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-07-25 11:11:05 +0530

    Update one.dart

--- one.dart
+++ one.dart
@@ -1,11 +1,13 @@
 // this library contains materials widget
 import "package:flutter/material.dart";
 
-
+// main method is an entry point of our app
 void main() {
-  // Text is a widget here
-  // Center is a widget
+
+  // inflates the widget & show it on app screen
   runApp(
+    // Text is a widget here
+    // Center is a widget
       Center(child: Text(
           "Hello Flutter",
           textDirection: TextDirection.ltr
