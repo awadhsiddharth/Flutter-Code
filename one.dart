257363745d1ee0526cfee7060b31df5d32d293b7
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-07-25 11:08:00 +0530

    Create one.dart

---
+++ one.dart
@@ -0,0 +1,15 @@
+// this library contains materials widget
+import "package:flutter/material.dart";
+
+
+void main() {
+  // Text is a widget here
+  // Center is a widget
+  runApp(
+      Center(child: Text(
+          "Hello Flutter",
+          textDirection: TextDirection.ltr
+      ),
+      )
+  );
+}
